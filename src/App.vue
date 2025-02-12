e5c6c73d782e5e4e8851bb5a16aa9965ab6e6ba3
Artem Ermakov <nawarius1993@gmail.com>
2025-02-12 18:25:04 +0400

    Init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+    import ThreeScene from './components/ThreeScene.vue';
+</script>
+
+<template>
+  <ThreeScene />
+</template>
+
+<style>
+* {
+    margin: 0px;
+    padding: 0px;
+}
+body {
+    overflow: hidden;
+}
+</style>
---
+++ src/components/ThreeScene.vue
@@ -0,0 +1,36 @@
+<script setup>
+    import * as THREE from 'three'
+    import { ref, onMounted } from 'vue'
+
+    const target = ref()
+
+    const scene = new THREE.Scene()
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+
+    const renderer = new THREE.WebGLRenderer()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    renderer.setAnimationLoop(animate)
+
+    
+
+    camera.position.z = 5
+
+    function animate() {
+        
+
+        renderer.render(scene, camera)
+    }
+
+    onMounted(() => {
+        target.value.appendChild(renderer.domElement)
+        animate()
+    })
+</script>
+
+<template>
+  <div ref="target" id = "three_container"></div>
+</template>
+
+<style>
+
+</style>
